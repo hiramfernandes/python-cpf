af46da1dc3980f340c1e86736ac60a463c42a28d
Hiram Fernandes <hiran.sd@gmail.com>
2023-01-24 21:20:10 +0000

    Added valida draft implementation

---
+++ valida.py
@@ -0,0 +1,21 @@
+from itertools import *
+
+def valida_cpf():
+    cpf = 96451963020
+    cpfChars = str(cpf)
+    if (len(cpfChars) == 11):
+        # Iterate through first 9 digits
+        print("Iterate through first 9 digits")
+        for index, value in enumerate(islice(cpfChars, 0, 9)):
+            print(f"{index} - {value}")
+
+        print("Iterate through first 10 digits")
+        # Iterate through first 10 digits (with the validation before added)
+        for index, value in enumerate(islice(cpfChars, 0, 10)):
+            print(f"{index} - {value}")
+    
+if __name__ == "__main__":
+    valida_cpf()
+
+def main():
+    valida_cpf()
