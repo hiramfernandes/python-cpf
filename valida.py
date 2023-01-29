eabfd5843920518a16cca9010ad3179a00f9c713
Hiram Fernandes <hiran.sd@gmail.com>
2023-01-29 00:07:35 +0000

    Versão funcional de valida digito

--- valida.py
+++ valida.py
@@ -12,16 +12,31 @@ def valida_cpf(cpf):
     # cpf = 96451963020
     cpfChars = str(cpf)
     if (len(cpfChars) == 11):
-        # Iterate through first 9 digits
-        print("Iterate through first 9 digits")
-        for index, value in enumerate(islice(cpfChars, 0, 9)):
-            print(f"{index} - {value}")
+        print(cpf)
 
-        print("Iterate through first 10 digits")
-        # Iterate through first 10 digits (with the validation before added)
-        for index, value in enumerate(islice(cpfChars, 0, 10)):
-            print(f"{index} - {value}")
+        digito1 = calcula_digito(cpf, 1)
+        digito2 = calcula_digito(cpf, 2)
+        
+        print(f"Digito 1: {digito1}")
+        print(f"Digito 2: {digito2}")
+
+
+def calcula_digito(cpf, digito):
+    numberOfIterations = 9 if digito == 1 else 10
+    indexMultiplier = 10 if digito == 1 else 11
+
+    cpfChars = str(cpf)
+    currentSum = 0
+    for index, value in enumerate(islice(cpfChars, 0, numberOfIterations)):
+        currentSum += int(value) * (indexMultiplier - index)
+
+    result = currentSum % 11
+
+    if (result > 2):
+        return 11- result
     
+    return 0
+
 
 def main(cpf_input):
     print(f'Execution from main method: Received {cpf_input}')
@@ -29,5 +44,9 @@ def main(cpf_input):
 
 
 if __name__ == "__main__":
-    cpf_input = sys.argv[1]
-    main(cpf_input=cpf_input)
+    try:
+        cpf_input = sys.argv[1]
+    except IndexError:
+        cpf_input = '96451963030'
+
+    main(cpf_input)
